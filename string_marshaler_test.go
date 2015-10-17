05d4ecbd0601ec0df7f3dd14ade8a63adc9859c8
Aleksey Filippov <sarum9in@gmail.com>
2015-10-17 04:18:47 +0400

    Added MarshalToString() and UnmarshalFromString().

---
+++ string_marshaler_test.go
@@ -0,0 +1,28 @@
+package encutil
+
+import (
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+type TestMarshaler string
+
+func (m *TestMarshaler) Marshal() ([]byte, error) {
+    return []byte(*m), nil
+}
+
+func (m *TestMarshaler) Unmarshal(data []byte) error {
+    *m = TestMarshaler(data)
+    return nil
+}
+
+func TestString(t *testing.T) {
+    var m TestMarshaler
+    err := UnmarshalFromString(&m, "hello, world")
+    assert.NoError(t, err)
+    assert.Equal(t, TestMarshaler("hello, world"), m)
+    data, err := MarshalToString(&m)
+    assert.NoError(t, err)
+    assert.Equal(t, "hello, world", data)
+}
